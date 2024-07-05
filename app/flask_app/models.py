ba779452c9d49062819d27c6c230e91dcc160120
pavel_rusakov <rusakov.pavel.perm@yandex.ru>
2024-07-06 00:51:13 +0500

    add models.py import typing dict for test flake8

--- app/flask_app/models.py
+++ app/flask_app/models.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DATETIME, Boolean, ForeignKey, Integer, String, UniqueConstraint
